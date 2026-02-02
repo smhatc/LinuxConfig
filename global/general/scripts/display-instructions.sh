1075d6c113f61f2187eef696552ef160e8222edc
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2026-02-02 03:09:19 +0300

    Moved instructions and assumptions to external text files which are outputted by the scripts for cleaner formatting than with echo

--- global/general/scripts/display-instructions.sh
+++ global/general/scripts/display-instructions.sh
@@ -1,21 +1,5 @@
 # Displaying instructions about optional script configurations
-echo -e "[IMPORTANT INSTRUCTIONS -- PLEASE READ]\n"
-echo -e "The script recommends creating and including the following optional configuration directories/files in some cases:\n"
-echo -e "1. An optional Microsoft fonts directory called \"microsoft\" in the path \"./global/general/fonts/microsoft\".\n"
-echo -e "-> If you wish to install Microsoft fonts, provide all the desired .ttf files in this directory.\n"
-echo -e "-> If the directory is not provided, the script will simply ignore this.\n"
-echo -e "2. An optional text file called \"trusted-connections-list.txt\" containing a list of"
-echo -e "trusted connection profile names (one per line) in the path \"./global/general/configurations/trusted-connections-list.txt\".\n"
-echo -e "-> This file will be used for firewall and DNS/IPv6 hardening to apply the trusted zone and its allowed services to trusted network connections.\n"
-echo "-> If the file is not provided, the script will simply ignore DNS/IPv6 configuration and apply restrictive firewall rules"
-echo -e "to all network interfaces (will break over-the-LAN apps and services).\n"
-echo "-> To harden the network configuration and avoid potential LAN problems, please first connect to all networks you trust (*),"
-echo -e "optionally renaming their connection profiles so they are easy to identify.\n"
-echo -e "* To connect to Wi-Fi from CLI:\n"
-echo -e "-> List nearby Wi-Fi networks: nmcli device wifi list\n"
-echo -e "-> Connect to a specific SSID: sudo nmcli device wifi connect \"<SSID>\" password \"<PASSWORD>\"\n"
-echo -e "-> To see current connection names: nmcli connection show\n"
-echo -e "-> To change current connection names: sudo nmcli connection modify \"<CURRENT-NAME>\" con-name \"<NEW-NAME>\"\n"
+cat ./global/general/other/instructions.txt
 
 echo -e "$line_separator\n"
 read -rp "Would you like to exit the script now to go create these directories/files? [Y/n] " exit_script
--- opensuse-tumbleweed/hyprland/scripts/display-assumptions.sh
+++ opensuse-tumbleweed/hyprland/scripts/display-assumptions.sh
@@ -1,14 +1,5 @@
 # Displaying assumptions the script expects from the environment it is being run in
-echo -e "\n[ASSUMPTIONS CONFIRMATION -- PLEASE READ]\n"
-echo -e "Because you have chosen to install \"${distro}\" with \"${desktop}\", the script assumes the following parameters were selected during OS installation:\n"
-echo -e "1. Enabled repositories: OSS, NON-OSS, Updates.\n"
-echo -e "2. System role: Server.\n"
-echo -e "3. A user has been created (user creation not skipped).\n"
-echo -e "4. The following pattern selection:\n"
-echo -e "-> Basic: Help and Support Documentation, Base System, Enhanced Base System, SELinux Support, x86-64-v3 Libraries, Mobile, Software Management, Minimal Appliance Base.\n"
-echo -e "-> Added (saves time during install, not needed): KVM Server/Tools, Fonts.\n"
-echo -e "-> Removed (saves time during cleanup, not needed): YaST Base Utilities, YaST Server Utilities.\n"
-echo -e "5. No login managers and other window managers/desktop environments installed.\n"
+cat ./opensuse-tumbleweed/hyprland/other/assumptions.txt
 
 echo -e "$line_separator\n"
 read -rp "Does your OS installation match the above description? [Y/n] " assumptions_correct
