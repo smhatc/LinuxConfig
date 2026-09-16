5e0bd29361007550b66466752598daa4259fcabe
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2026-09-16 18:53:06 +0300

    Ported hypr/modules/environment-variables.conf to Lua

--- global/general/configurations/.config/hypr/modules/environment-variables.lua
+++ global/general/configurations/.config/hypr/modules/environment-variables.lua
@@ -1,8 +1,21 @@
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
+-----------------------------
+--- ENVIRONMENT VARIABLES ---
+-----------------------------
 
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+-- Setting environment variables specifically relevant to Hyprland
 
+-- ssh-agent socket for systemd user unit
+hl.env("SSH_AUTH_SOCK", os.getenv("XDG_RUNTIME_DIR") .. "/ssh-agent.socket")
+
+-- Explicitly setting XDG variables
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+
+-- UI toolkit backends and theming (prefer Wayland, scaling, and theming)
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
+hl.env("XCURSOR_THEME", "Adwaita")
 hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
