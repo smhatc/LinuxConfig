2533b2acbebf16db3003b45badf15c41112a6bd2
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2026-09-16 19:01:21 +0300

    Ported hypr/modules/permissions.conf to Lua

--- global/general/configurations/.config/hypr/modules/permissions.lua
+++ global/general/configurations/.config/hypr/modules/permissions.lua
@@ -1,17 +1,25 @@
------------------------
------ PERMISSIONS -----
------------------------
+-------------------
+--- PERMISSIONS ---
+-------------------
 
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
--- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
--- for security reasons
+-- Utilizing the Hyprland permission system to restrict programs' system access
 
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
+-- Enforce Hyprland compositor permissions (requires Hyprland restart to apply, format: path/regex, permission, mode)
+hl.config({
+    ecosystem = {
+        enforce_permissions = true,
+    },
+})
 
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+-- "screencopy" permission (access to screen without going through xdg-desktop-portal-hyprland)
+hl.permission({ binary = "/usr/(bin|local/bin)/grim", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/(bin|local/bin)/hyprpicker", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/(bin|local/bin)/hyprlock", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
+hl.permission({ binary = ".*", type = "screencopy", mode = "deny" })
+
+-- "plugin" permission (access to load a plugin)
+hl.permission({ binary = ".*", type = "plugin", mode = "deny" })
+
+-- "keyboard" permission (access to connecting a new keyboard)
+hl.permission({ binary = ".*", type = "keyboard", mode = "allow" })
