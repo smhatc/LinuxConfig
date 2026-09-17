0fae8d1a31a44c346dbf102a1f9849c47344323c
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2026-09-17 20:57:24 +0300

    Added Docker/Compose to Tumbleweed Zypper apps and service/group scripts

--- global/general/scripts/configure-services.sh
+++ global/general/scripts/configure-services.sh
@@ -1,5 +1,6 @@
 # Enabling any needed system services
-echo "${process_icon} Enabling any needed system services (firewalld, libvirtd)..."
+echo "${process_icon} Enabling any needed system services (firewalld, libvirtd, docker)..."
 sudo systemctl enable --now firewalld.service
 sudo systemctl enable --now libvirtd.service
-echo "${success_icon} Finished enabling any needed system services (firewalld, libvirtd)."
+sudo systemctl enable --now docker.service
+echo "${success_icon} Finished enabling any needed system services (firewalld, libvirtd, docker)."
--- global/general/scripts/configure-users-groups.sh
+++ global/general/scripts/configure-users-groups.sh
@@ -1,4 +1,4 @@
 # Add current user to any needed groups
-echo "${process_icon} Adding the current user to any needed groups (libvirt, wireshark)..."
-sudo usermod -aG libvirt,wireshark "$(whoami)"
-echo "${success_icon} Finished adding the current user to any needed groups (libvirt, wireshark)."
+echo "${process_icon} Adding the current user to any needed groups (libvirt, wireshark, docker)..."
+sudo usermod -aG libvirt,wireshark,docker "$USER"
+echo "${success_icon} Finished adding the current user to any needed groups (libvirt, wireshark, docker)."
