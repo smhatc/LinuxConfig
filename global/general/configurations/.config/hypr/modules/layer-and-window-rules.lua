433d27c21be2743f522b19c726b36ba404cd2abe
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2026-09-19 02:52:05 +0300

    Ported hypr/modules/layer-and-window-rules.conf to Lua

--- global/general/configurations/.config/hypr/modules/layer-and-window-rules.lua
+++ global/general/configurations/.config/hypr/modules/layer-and-window-rules.lua
@@ -1,49 +1,63 @@
---------------------------------
----- WINDOWS AND WORKSPACES ----
---------------------------------
+-------------------
+--- LAYER RULES ---
+-------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
--- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- Defining layers' behavior (use "hyprctl layers" to find layers' attributes)
 
--- Example window rules that are useful
+-- Fix random border appearing in screenshots
+hl.layer_rule({
+    name = "fix-screenshot-border-appearing",
 
-local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
+    match = { namespace = "selection" },
+
+    no_anim = true,
+})
+
+-- Dim everything around layers
+hl.layer_rule({
+    name = "dim-around-layers",
+
+    match = { namespace = "(rofi|swaync-control-center)" },
+
+    dim_around = true,
+})
+
+--------------------
+--- WINDOW RULES ---
+--------------------
+
+-- Defining windows' behavior (use "hyprctl clients" to find windows' attributes)
+
+-- Ignore maximize requests from all windows
+hl.window_rule({
+    name = "suppress-maximize-events",
+
     match = { class = ".*" },
 
     suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
 
+-- Fix some dragging issues with XWayland
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
+    name = "fix-xwayland-drags",
+
     match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
+        class = "^$",
+        title = "^$",
+        xwayland = true,
+        float = true,
         fullscreen = false,
-        pin        = false,
+        pin = false,
     },
 
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
+-- Float specific windows
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+    name = "float-specific-windows",
 
-    move  = "20 monitor_h-120",
+    match = { class = "(^hyprland-.*$|xdg-desktop-portal-gtk|udiskie|org.bleachbit.BleachBit|org.gnome.Characters)" },
+
     float = true,
 })
